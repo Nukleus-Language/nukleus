e66fdb121461770949f8a0c7ac3b23c120dcf4f7
Skuld Norniern <skuldnorniern@gmail.com>
2022-12-26 17:08:11 +0900

    [IMPROVE] Improved readability of the files

--- src/core/ast_temp/ast.rs
+++ src/core/ast_temp/ast.rs
@@ -17,7 +17,7 @@ pub enum AST {
         parameters: Vec<String>,
         body: Box<AST>,
     },
-    
+
     Let {
         name: String,
         type_name: Option<String>,
@@ -28,9 +28,35 @@ impl PartialEq for AST {
     fn eq(&self, other: &Self) -> bool {
         match (self, other) {
             (AST::Token(t1), AST::Token(t2)) => t1 == t2,
-            (AST::Import { path: p1, name: n1 }, AST::Import { path: p2, name: n2 }) => p1 == p2 && n1 == n2,
-            (AST::Function { public: p1, name: n1, parameters: par1, body: b1 }, AST::Function { public: p2, name: n2, parameters: par2, body: b2 }) => p1 == p2 && n1 == n2 && par1 == par2 && b1 == b2,
-            (AST::Let { name: n1, type_name: tn1, value: v1 }, AST::Let { name: n2, type_name: tn2, value: v2 }) => n1 == n2 && tn1 == tn2 && v1 == v2,
+            (AST::Import { path: p1, name: n1 }, AST::Import { path: p2, name: n2 }) => {
+                p1 == p2 && n1 == n2
+            }
+            (
+                AST::Function {
+                    public: p1,
+                    name: n1,
+                    parameters: par1,
+                    body: b1,
+                },
+                AST::Function {
+                    public: p2,
+                    name: n2,
+                    parameters: par2,
+                    body: b2,
+                },
+            ) => p1 == p2 && n1 == n2 && par1 == par2 && b1 == b2,
+            (
+                AST::Let {
+                    name: n1,
+                    type_name: tn1,
+                    value: v1,
+                },
+                AST::Let {
+                    name: n2,
+                    type_name: tn2,
+                    value: v2,
+                },
+            ) => n1 == n2 && tn1 == tn2 && v1 == v2,
             _ => false,
         }
     }
--- src/main.rs
+++ src/main.rs
@@ -14,7 +14,7 @@ fn read_file(filename: &str) -> Result<String, std::io::Error> {
 fn main() {
     //let contents = read_file("input.nk").unwrap();
     let contents = "let<int> a = 3;";
-    let tokens = core::lexer::lexer(&contents);
+    let tokens = core::lexer::lexer(contents);
     println!("{:?}", tokens);
     let ast = core::parser_new::parse::Parser::new(tokens).parse();
     println!("{:?}", ast);
