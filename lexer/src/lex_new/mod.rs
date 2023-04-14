04de3b78cfa18f80be3e42406012384e83818b0c
SkuldNorniern <skuldnorniern@gmail.com>
2023-04-14 10:03:34 +0000

    [ADD] Added quotedstring lexing

--- lexer/src/lex_new/mod.rs
+++ lexer/src/lex_new/mod.rs
@@ -9,19 +9,18 @@ use crate::tokens_new::*;
 
 #[derive(Debug, Clone, PartialEq)]
 enum State {
+    StateEmpty,
     StateDefault,
     Number,
     Identifier,
-    Operator,
-    Symbol,
-    QuoatedString,
-    Comment,
-    DoubleSymbol
+    QuotedString,
+    DoubleState
 }
 
 struct Lexer<'a> {
     code: Peekable<Chars<'a>>,
     tokens: Vec<Token>,
+    state:  State,
     buffer: String,
     line: usize,
     column: usize,
@@ -32,6 +31,7 @@ impl<'a> Lexer<'a> {
         Lexer {
             code: code.chars().peekable(),
             tokens: Vec::new(),
+            state: State::StateEmpty,
             buffer: String::new(),
             line: 1,
             column: 1,
@@ -40,26 +40,49 @@ impl<'a> Lexer<'a> {
     fn run(&mut self) {
         let mut state = State::StateDefault;
         while let Some(c) = self.next_char() {
+
+            println!("Current Char: {}", c);
+            println!("Current State: {:?}", state);
+            println!("Current Buffer: {}", self.buffer);
+            if state == State::DoubleState{
+                self.buffer.clear();
+                State::StateEmpty;
+                continue;
+            }
+            if is_whitespace(c) && state != State::QuotedString {
+                self.buffer.clear();
+                state = State::StateEmpty;
+                continue;
+            }
             // Check if the buffer is empty and the current character when is empty
             if self.buffer.is_empty(){
+                // run the symbol_to token to check if is a symbol
+                let symbol = symbol_to_token(c, self.line, self.column);
+                match symbol {
+                    Ok(symbol) => {
+                        self.insert_token(symbol);
+                        continue;
+                    }
+                    Err(_) => {}
+                }
                 self.buffer.push(c);
+                
                 state = State::StateDefault;
                 continue;
             }
-
+            println!("Current First Char: {}", self.buffer.chars().nth(0).unwrap());
+            
             // if the first character is a - or number and the next character is a number
             // then it is a number
             let first_char = self.buffer.chars().nth(0).unwrap();
-            if state == State::StateDefault && ((first_char == '-' || is_numeric(first_char)) && is_numeric(*self.peek_char().unwrap())) {
+            if state == State::StateDefault && ((first_char == '-' || is_numeric(first_char)) && is_numeric(self.peek_char())) {
                 state = State::Number;
                 self.buffer.push(c);
-                continue;
             }
-            if state == State::Number && is_numeric(c) {
+            else if state == State::Number && is_numeric(c) {
                 self.buffer.push(c);
-                continue;
             }
-            else if state == State::Number && !is_numeric(c) {
+            if state == State::Number && !is_numeric(self.peek_char()) {
                 //let number = buffer.parse::<i32>().unwrap();
                 let number = number_to_token(self.buffer.clone(), self.line, self.column);
                 match number {
@@ -67,9 +90,63 @@ impl<'a> Lexer<'a> {
                     Err(error) => self.report_error(error),
                 }
                 self.buffer.clear();
-                state = State::StateDefault;
+                state = State::StateEmpty;
                 continue;
             }
+
+            // if the first character is a " then it is a string
+            if state == State::StateDefault && is_quote(first_char) {
+                state = State::QuotedString;
+                self.buffer.push(c);
+                continue;
+            }
+            else if state == State::QuotedString && !is_quote(c) {
+                self.buffer.push(c);
+                continue;
+            }
+            else if state == State::QuotedString && is_quote(c) {
+                let string = self.buffer.clone();
+                self.buffer.push(c);
+                // trim the quotes
+                let string = string.trim_matches('"').to_string();
+                self.insert_token(Token::TypeValue(TypeValue::QuotedString(string)));
+                self.buffer.clear();
+                state = State::StateEmpty;
+                continue;
+            }
+            
+
+
+            // check if is a identifier, statement, or symbol
+            if state == State::StateDefault && is_first_identifierable(first_char) {
+                state = State::Identifier;
+                self.buffer.push(c);
+            }
+            else if state == State::Identifier && is_identifierable(c) {
+                self.buffer.push(c);
+                
+            }
+            if state == State::Identifier {
+                //let identifier = identifier_to_token(self.buffer.clone(), self.line, self.column);
+                let statement = statement_to_token(self.buffer.clone(), self.line, self.column);
+                match statement {
+                    Ok(statement) => {
+                        self.insert_token(statement);
+                        self.buffer.clear();
+                        state = State::StateEmpty;
+                        continue;
+                    }
+                    Err(_) => {}
+                }
+                /*match identifier {
+                    Ok(identifier) => self.insert_token(identifier),
+                    Err(error) => self.report_error(error),
+                }*/
+                //self.buffer.clear();
+                //state = State::StateEmpty;
+                continue;
+            }
+
         }
     }
     fn next_char(&mut self) -> Option<char> {
@@ -78,29 +155,38 @@ impl<'a> Lexer<'a> {
             Some('\n') => {
                 self.line += 1;
                 self.column = 1;
-                self.next_char();
+                //println!("Line Change: {}", self.line);
+                //println!("Column Change: {}", self.column);
             }
             Some(' ') => {
                 self.column += 1;
-                self.next_char();
+                //println!("Column Change: {}", self.column);
+                
             }
             Some('\t') => {
                 self.column += 4;
-                self.next_char();
+                //println!("Column Change: {}", self.column);
             }
-            Some(_) => self.column += 1,
+            Some(_) => {
+                self.column += 1;
+                //println!("Column Change: {}", self.column);
+            }
             None => {}
         }
         next
     }
-    fn peek_char(&mut self) -> Option<&char> {
-        self.code.peek()
+    fn peek_char(&mut self) -> char {
+        let peek = self.code.peek();
+        match peek{
+            Some(_) => peek.unwrap().clone(),
+            None => ' '
+        }
     }
     fn insert_token(&mut self, token: Token) {
         self.tokens.push(token);
     }
     fn report_error(&self, error: LexcialError) {
-        println!("Lexial Error: {}", error);
+        print!("{} on ", error);
         println!("Line: {}, Column: {}", self.line, self.column);
     }
 }
@@ -113,6 +199,12 @@ fn is_alpha(c: char) -> bool {
 }
 fn is_whitespace(c: char) -> bool {
     c.is_whitespace()
+}
+fn is_quote(c: char) -> bool {
+    match c {
+        '"' => true,
+        _ => false,
+    }
 }
 fn is_operator(c: char) -> bool {
     match c {
@@ -132,19 +224,19 @@ fn is_quoted_string(c: char) -> bool {
         _ => false,
     }
 }
-fn is_statement(b: String) -> bool{
-    match b.as_str() {
-        "if" | "else" | "while" | "for" | "return" | "break" | "continue" | "let" | "const" | "fn" | "public" | "import"  => true,
-        _ => false,
-    }
+fn is_identifierable(c: char) -> bool {
+    is_alpha(c) || is_numeric(c) || c == '_'
 }
-fn operator_to_token(operator: String, line:usize, column:usize) -> Result<Token, LexcialError> {
-    match operator.as_str() {
-        "+" => Ok(Token::Operator(Operator::Add)),
-        "-" => Ok(Token::Operator(Operator::Subtract)),
-        "*" => Ok(Token::Operator(Operator::Multiply)),
-        "/" => Ok(Token::Operator(Operator::Divide)),
-        "%" => Ok(Token::Operator(Operator::Remainder)),
+fn is_first_identifierable(c: char) -> bool {
+    is_alpha(c) || c == '_'
+}
+fn operator_to_token(operator: char, line:usize, column:usize) -> Result<Token, LexcialError> {
+    match operator {
+        '+' => Ok(Token::Operator(Operator::Add)),
+        '-' => Ok(Token::Operator(Operator::Subtract)),
+        '*' => Ok(Token::Operator(Operator::Multiply)),
+        '/' => Ok(Token::Operator(Operator::Divide)),
+        '%' => Ok(Token::Operator(Operator::Remainder)),
         _ => return Err(LexcialError{line, column, message: LexError::InvalidOperator(operator.to_string())}),
     }
 }
@@ -157,27 +249,30 @@ fn number_to_token(number: String, line:usize, column:usize) -> Result<Token, Le
         Err(_) => return Err(LexcialError{line, column, message: LexError::InvalidNumber(number.to_string())}),
     }
 }
-fn syambol_to_token(symbol: String, line:usize, column:usize) -> Result<Token, LexcialError>{
-    match symbol.as_str() {
-        "@" => Ok(Token::Symbol(Symbol::At)),
-        ":" => Ok(Token::Symbol(Symbol::Colon)),
-        "." => Ok(Token::Symbol(Symbol::Dot)),
-        "=" => Ok(Token::Assign(Assign::Assign)),
+//fn quoted_string_to_token(string: String, line:usize, column:usize) -> Result<Token, LexcialError> {
+//    Ok(Token::String(string))
+//}
+fn symbol_to_token(symbol: char, line:usize, column:usize) -> Result<Token, LexcialError>{
+    match symbol {
+        ',' => Ok(Token::Symbol(Symbol::Comma)),
+        ':' => Ok(Token::Symbol(Symbol::Colon)),
+        '.' => Ok(Token::Symbol(Symbol::Dot)),
+        '=' => Ok(Token::Assign(Assign::Assign)),
         //'-' => Token::Operator(Operator::Subtract),
-        "(" => Ok(Token::Symbol(Symbol::OpenParen)),
-        "{" => Ok(Token::Symbol(Symbol::OpenBrace)),
-        "<" => Ok(Token::Logical(Logical::LessThan)),
-        "[" => Ok(Token::Symbol(Symbol::OpenSquare)),
-        ")" => Ok(Token::Symbol(Symbol::CloseParen)),
-        "}" => Ok(Token::Symbol(Symbol::CloseBrace)),
-        ">" => Ok(Token::Logical(Logical::GreaterThan)),
-        "]" => Ok(Token::Symbol(Symbol::CloseSquare)),
+        '(' => Ok(Token::Symbol(Symbol::OpenParen)),
+        '{' => Ok(Token::Symbol(Symbol::OpenBrace)),
+        //"<' => Ok(Token::Logical(Logical::LessThan)),
+        '[' => Ok(Token::Symbol(Symbol::OpenSquare)),
+        ')' => Ok(Token::Symbol(Symbol::CloseParen)),
+        '}' => Ok(Token::Symbol(Symbol::CloseBrace)),
+        //'>' => Ok(Token::Logical(Logical::GreaterThan)),
+        ']' => Ok(Token::Symbol(Symbol::CloseSquare)),
         //'+' => Token::Operator(Operator::Add),
         //'%' => Token::Operator(Operator::Remainder),
-        ";" => Ok(Token::Symbol(Symbol::Semicolon)),
-        "/" => Ok(Token::Operator(Operator::Divide)),
-        "," => Ok(Token::Symbol(Symbol::Comma)),
-        "!" => Ok(Token::Logical(Logical::Not)),
+        ';' => Ok(Token::Symbol(Symbol::Semicolon)),
+        '/' => Ok(Token::Operator(Operator::Divide)),
+        //"," => Ok(Token::Symbol(Symbol::Comma)),
+        //"!" => Ok(Token::Logical(Logical::Not)),
         _ => return Err(LexcialError{line, column, message: LexError::InvalidSymbol(symbol.to_string())}),
     }
 }
@@ -212,6 +307,22 @@ fn statement_to_token(statement:String, line:usize, column:usize) -> Result<Toke
                 _ => return Err(LexcialError{line, column, message: LexError::InvalidStatement(statement.to_string())}),
        }
 }
+fn typename_to_token(typename:String, line:usize, column:usize) -> Result<Token, LexcialError>{
+    match typename.as_str() {
+        "void" => Ok(Token::TypeName(TypeName::Void)),
+        "bool" => Ok(Token::TypeName(TypeName::Bool)),
+        "string" => Ok(Token::TypeName(TypeName::QuotedString)),
+        "i8" => Ok(Token::TypeName(TypeName::I8)),
+        "i16" => Ok(Token::TypeName(TypeName::I16)),
+        "i32" => Ok(Token::TypeName(TypeName::I32)),
+        "i64" => Ok(Token::TypeName(TypeName::I64)),
+        "u8" => Ok(Token::TypeName(TypeName::U8)),
+        "u16" => Ok(Token::TypeName(TypeName::U16)),
+        "u32" => Ok(Token::TypeName(TypeName::U32)),
+        "u64" => Ok(Token::TypeName(TypeName::U64)),
+        _ => return Err(LexcialError{line, column, message: LexError::InvalidTypeName(typename.to_string())}),
+    }
+}
 
 
 #[cfg(test)]
@@ -219,20 +330,36 @@ mod test{
     use super::*;
     #[test]
     fn line_counting(){
-        let code = "fn main() -> void {
-            println(\"Hello, world!\");
-        }";
+        let code = "fn main() -> void \n{\nprintln(\"Hello, world!\");\n}";
         let mut lexer = Lexer::new(code);
         lexer.run();
-        assert_eq!(lexer.line, 3);
+        println!("{:?}", lexer.tokens);
+        assert_eq!(lexer.line, 4);
     }
     #[test]
     fn column_counting(){
-        let code = "fn main() -> void {
-            println(\"Hello, world!\");
-        }";
+        let code = "fn main() -> void\n{\nprintln(\"Hello, world!\");\n}";
         let mut lexer = Lexer::new(code);
         lexer.run();
+        println!("{:?}", lexer.tokens);
         assert_eq!(lexer.column, 2);
     }
+    #[test]
+    fn lexing_numbers(){
+        let code = "fn main() -> void \n{\nlet:i32 a = 5;\nlet:i32 b = 10;\n}";
+        let mut lexer = Lexer::new(code);
+        lexer.run();
+        println!("{:?}", lexer.tokens);
+        panic!();
+    }
+    #[test]
+    fn lexing_strings(){
+        let code = " \"Hello, world!\" ";
+        let ans = vec![Token::TypeValue(TypeValue::QuotedString("Hello, world!".to_string()))];
+        let mut lexer = Lexer::new(code);
+        lexer.run();
+        println!("{:?}", lexer.tokens);
+        assert_eq!(lexer.tokens, ans);
+    }
 }
+
