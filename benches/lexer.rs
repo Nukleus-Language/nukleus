7f5cba024f98c56bfed40c1efeeeeb2fa943b095
SkuldNorniern <skuldnorniern@gmail.com>
2023-10-23 20:06:39 +0900

    feat(bench): add lexer bench for getting speed of lexer

--- benches/lexer.rs
+++ benches/lexer.rs
@@ -1,1 +1,31 @@
+use divan::{Bencher, counter::{BytesCount, ItemsCount}};
+const TEST_CODE: &str = include_str!("../test.nk");
 
+fn main() {
+    divan::main()
+}
+
+#[divan::bench(counters = [
+    BytesCount::of_slice(TEST_CODE),
+    ItemsCount::new(TEST_CODE.len()),
+],threads = [0, 1, 4, 8, 16])]
+fn new_lexer(){
+
+    fn compute(code: &str) {
+        let mut lexer = lexer::lex_new::Lexer::new(code);
+        let tokens = lexer.run();
+    }
+    compute(divan::black_box(TEST_CODE))       
+}
+
+#[divan::bench(counters = [
+    BytesCount::of_slice(TEST_CODE),
+    ItemsCount::new(TEST_CODE.len()),
+],threads = [0, 1, 4, 8, 16])]
+fn old_lexer(){
+
+    fn compute(code: &str) {
+        let tokens= lexer::lexer(code);
+    }
+    compute(divan::black_box(TEST_CODE))       
+}
