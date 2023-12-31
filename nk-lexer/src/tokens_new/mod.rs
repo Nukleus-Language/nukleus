65bc1e146e28b47e586ddddb271210f139cf95fb
SkuldNorniern <skuldnorniern@gmail.com>
2023-12-31 18:20:37 +0900

    feat: introduce metadata for tokens

--- nk-lexer/src/tokens_new/mod.rs
+++ nk-lexer/src/tokens_new/mod.rs
@@ -4,6 +4,7 @@ mod symbols;
 mod types;
 
 use std::fmt;
+// use std::path::PathBuf;
 
 pub use operators::Assign;
 pub use operators::Logical;
@@ -16,12 +17,24 @@ pub use types::TypeValue;
 
 pub use symbols::Symbol;
 
-// FEAT:TASK: add trait for the Tokens for error line, pos support
+#[derive(Debug, Clone, PartialEq, Eq, Hash, Copy, Default)]
+pub struct TokenMetadata {
+    pub line: usize,
+    pub column: usize,
+}
 
+impl TokenMetadata {
+    pub fn new(line: usize, column: usize /* , src_location: Option<PathBuf> */) -> Self {
+        TokenMetadata { line, column }
+    }
+}
+impl fmt::Display for TokenMetadata {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "line: {}, column: {}", self.line, self.column)
+    }
+}
 #[derive(Debug, Clone, PartialEq, Eq, Hash)]
-#[allow(missing_docs)]
-#[allow(dead_code)]
-pub enum Token {
+pub enum TokenType {
     Operator(Operator),
     Logical(Logical),
     Assign(Assign),
@@ -32,17 +45,37 @@ pub enum Token {
     EOF,
 }
 
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+pub struct Token {
+    pub token_type: TokenType,
+    pub metadata: TokenMetadata,
+}
+impl Token {
+    pub fn new(token_type: TokenType, metadata: TokenMetadata) -> Self {
+        Token {
+            token_type,
+            metadata,
+        }
+    }
+}
+
 impl fmt::Display for Token {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", self.token_type)
+    }
+}
+
+impl fmt::Display for TokenType {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match self {
-            Token::Operator(op) => write!(f, "{}", op),
-            Token::Logical(log) => write!(f, "{}", log),
-            Token::Assign(asn) => write!(f, "{}", asn),
-            Token::Statement(st) => write!(f, "{}", st),
-            Token::TypeName(tn) => write!(f, "{}", tn),
-            Token::TypeValue(tv) => write!(f, "{}", tv),
-            Token::Symbol(sym) => write!(f, "{}", sym),
-            Token::EOF => write!(f, "EOF"),
+            TokenType::Operator(op) => write!(f, "{}", op),
+            TokenType::Logical(log) => write!(f, "{}", log),
+            TokenType::Assign(assign) => write!(f, "{}", assign),
+            TokenType::Statement(stat) => write!(f, "{}", stat),
+            TokenType::TypeName(typename) => write!(f, "{}", typename),
+            TokenType::TypeValue(typeval) => write!(f, "{}", typeval),
+            TokenType::Symbol(symbol) => write!(f, "{}", symbol),
+            _ => write!(f, "EOF"),
         }
     }
 }
